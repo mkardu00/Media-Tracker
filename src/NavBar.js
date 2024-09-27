9c7097dbfe39971c449147f6e58cba93817eabf2
marijakilic <mkardu00@fesb.hr>
2024-09-27 09:30:58 +0200

    Add profile img

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,12 +1,18 @@
 import React from "react";
-import { Link, useLocation } from "react-router-dom";
-
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import profileImg from "./assets/profile.png";
 import "./NavBar.css";
 
 const NavBar = () => {
   const location = useLocation();
+  const navigate = useNavigate();
   const isHomePage = location && location.pathname === "/";
   const currentUser = localStorage.getItem("currentUser");
+
+  const handleSignOut = () => {
+    localStorage.removeItem("currentUser");
+    navigate("/");
+  };
 
   return (
     <div>
@@ -55,9 +61,21 @@ const NavBar = () => {
             </li>
 
             {currentUser && (
-              <li>
-                <Link>Hello, {currentUser}</Link>
-              </li>
+              <>
+                <li className="user-info">
+                  <img
+                    src={profileImg}
+                    alt="User Avatar"
+                    className="user-avatar"
+                  />
+                  <span className="greeting">Hello, {currentUser}</span>
+                </li>
+                <li>
+                  <button onClick={handleSignOut} className="sign-out-button">
+                    Sign Out
+                  </button>
+                </li>
+              </>
             )}
           </ul>
         </nav>
